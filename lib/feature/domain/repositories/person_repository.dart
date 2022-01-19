0868152148a2984fcc04650404a84d819661c789
Yaroslav <hodosov.y@mail.ru>
2022-01-19 22:38:54 +0300

    add usecases

--- lib/feature/domain/repositories/person_repository.dart
+++ lib/feature/domain/repositories/person_repository.dart
@@ -3,6 +3,6 @@ import 'package:rick_and_morty/core/error/failure.dart';
 import 'package:rick_and_morty/feature/domain/enities/porson_entity.dart';
 
 abstract class PersonRepository {
-  Future<Either<Failure, List<PersonEntity>>> getAllPersons(intPage);
+  Future<Either<Failure, List<PersonEntity>>> getAllPersons(int Page);
   Future<Either<Failure, List<PersonEntity>>> searchPerson(String query);
 }
